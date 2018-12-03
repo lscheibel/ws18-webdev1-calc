5630ceb3038f77b88298171608e7dcdcce80cd6c
Lennard Scheibel <44374653+lscheibel@users.noreply.github.com>
2018-12-03 11:33:44 +0100

    Support more operands and use ES6

--- index.js
+++ index.js
@@ -2,19 +2,26 @@
  * Reverse Polish Notation Calculator
  */
 
-var arguments = process.argv.slice(2);
-
-var operand1 = arguments[0];
-var operand2 = arguments[1];
-var operator = arguments[2];
+let arguments = process.argv.slice(2);
+let [operand1, operand2, operator] = arguments;
 
 console.log('Calculating:', operand1, operand2, operator);
 
-var result;
+let result;
 
 switch (operator) {
   case '+':
     result = operand1 + operand2;
+    break;
+  case '-':
+    result = operand1 - operand2;
+    break;
+  case '*':
+    result = operand1 * operand2;
+    break;
+  case '/':
+    result = operand1 / operand2;
+    break;
   default:
     console.error('Not implemented:', operator);
 }
